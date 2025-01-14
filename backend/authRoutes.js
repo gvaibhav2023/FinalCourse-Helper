20bfca23d35a4cf1f5ab0440ee315a7d6ccdf37e
Vaibhav <vaibhav27092005@gmail.com>
2025-01-14 10:15:25 +0530

    Ignore .next folder

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -10,7 +10,7 @@ router.post(
   [
     body("username").notEmpty().withMessage("Username is required."),
     body("password")
-      .isLength({ min: 4 })
+      .isLength({ min: 6 })
       .withMessage("Password must be at least 6 characters long."),
   ],
   signup
