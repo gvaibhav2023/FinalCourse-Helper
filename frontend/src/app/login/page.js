eda7e38849b5b76f9bfd4cfab7a2b351cfaf7ea2
Vaibhav <vaibhav27092005@gmail.com>
2025-01-15 18:25:04 +0530

    maybe last commit

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -22,7 +22,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://final-course-helper-b41a.vercel.app/auth/login", {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
